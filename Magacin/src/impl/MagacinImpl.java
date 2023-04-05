3faf2dc4f38316e81b50f281f4ba3d2009c3df42
profara <alekbg99@gmail.com>
2023-04-05 19:32:43 +0200

    Dodata impl

--- Magacin/src/impl/MagacinImpl.java
+++ Magacin/src/impl/MagacinImpl.java
@@ -9,22 +9,35 @@ import magacin.Artikal;
 public class MagacinImpl implements Magacin{
 
 	
+	private Artikal artikal;
+	private List<Artikal> artikli;
+	
+	public MagacinImpl(Artikal artikal) {
+		this.artikal = artikal;
+		this.artikli = new ArrayList<>();
+	}
+	
 	@Override
 	public void dodajArtikal() {
 		
+		artikal.setKolicina(artikal.getKolicina()+1);
 		
 	}
 
 	@Override
 	public void izbaciArtikal() {
 		
-		
+		artikal.setKolicina(artikal.getKolicina()-1);
 		
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-
+		
+		for(Artikal artikal: artikli) {
+			if(artikal.getSifra() == sifra)
+				return artikal;
+		}
 		
 		return null;
 		
