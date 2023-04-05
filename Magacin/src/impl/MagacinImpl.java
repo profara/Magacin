896f3896a047ec2e7d013f20cd4a82c2bce59801
profara <alekbg99@gmail.com>
2023-04-05 13:28:49 +0200

    Dodata klasa MagacinImpl koja implementira interfejs Magacin

---
+++ Magacin/src/impl/MagacinImpl.java
@@ -0,0 +1,26 @@
+package impl;
+
+import interfejs.Magacin;
+import magacin.Artikal;
+
+public class MagacinImpl implements Magacin{
+
+	@Override
+	public void dodajArtikal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
